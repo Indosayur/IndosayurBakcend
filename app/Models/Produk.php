28fba166cd536dd774a70adc4c259da62c1317f7
Indosayur <Indosayur.indonesia@gmail.com>
2021-06-19 04:51:57 +0800

    Initial Commit

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -2,12 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Produk extends Model
 {
-    use HasFactory;
     protected $fillable = [
         'name', 'harga', 'deskripsi', 'kategori_id','gambar'
     ];
